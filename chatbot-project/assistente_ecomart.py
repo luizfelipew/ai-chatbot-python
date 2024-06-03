950c1c360c0d8180f200d9dcd9c4c548d07c6d56
luizfelipew <luizfelipewendt@gmail.com>
2024-06-03 20:05:25 -0300

    integrando o chatbot

--- chatbot-project/assistente_ecomart.py
+++ chatbot-project/assistente_ecomart.py
@@ -11,50 +11,26 @@ cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = "gpt-4"
 contexto = carrega("dados/ecomart.txt")
 
-assistente = cliente.beta.assistants.create(
-    name="Atendente EcoMart",
-    instructions=f"""
-        Você é um chatbot de atendimento a clientes de um e-commerce. 
-        Você não deve responder perguntas que não sejam dados do ecommerce informado!
-        Além disso, adote a persona abaixo para responder ao cliente.
-        
-        ## Contexto
-        {contexto}
-        
-        ## Persona
-        
-        {personas["neutro"]}
-    """,
-    model=modelo,
-)
 
-thread = cliente.beta.threads.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Liste os produtos"
-        }
-    ]
-)
+def criar_thread():
+    return cliente.beta.threads.create()
 
-cliente.beta.threads.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content=" da categoria moda sustentável"
-)
 
-run = cliente.beta.threads.runs.create(
-    thread_id=thread.id,
-    assistant_id=assistente.id
-)
-
-while run.status != "completed":
-    run = cliente.beta.threads.runs.retrieve(
-        thread_id=thread.id,
-        run_id=run.id
+def criar_assistente():
+    assistente = cliente.beta.assistants.create(
+        name="Atendente EcoMart",
+        instructions=f"""
+            Você é um chatbot de atendimento a clientes de um e-commerce. 
+            Você não deve responder perguntas que não sejam dados do ecommerce informado!
+            Além disso, adote a persona abaixo para responder ao cliente.
+            
+            ## Contexto
+            {contexto}
+            
+            ## Persona
+            
+            {personas["neutro"]}
+        """,
+        model=modelo,
     )
-
-historico = cliente.beta.threads.messages.list(thread_id=thread.id).data
-
-for mensagem in reversed(historico):
-    print(f"role: {mensagem.role}\nConteúdo: {mensagem.content[0].text.value}")
+    return assistente
